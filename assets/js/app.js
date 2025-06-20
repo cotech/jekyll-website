876e7cd0c8782d624b97d91698ca00a5e0bf29a6
Chris Lowis <chris.lowis@gmail.com>
2025-06-20 13:12:21 +0100

    Remove client-list shuffle javascript
    The selector in this JS doesn't seem to match anything on the site, so
    I'm removing it.

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,10 +1,3 @@
 $(document).foundation();
 
 var elem = new Foundation.Sticky($('.top-bar'));
-
-//# sourceMappingURL=app.js.map
-$('#client-list .column').shuffle();
-window.setInterval(function(){
-    /// call your function here
-    $('#client-list .column').shuffle();
-}, 7000);
